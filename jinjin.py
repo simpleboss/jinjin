f4f27cc7c8df5cb03e4f34019107bc4a922788ca
simpleboss <gichan.lee@threemindev.com>
2019-07-31 14:07:25 +0700

    output

--- jinjin.py
+++ jinjin.py
@@ -1,8 +1,17 @@
 # input
-# total_number = int(input())
-# input_list = []
-# for i in range(total_number):
-#     a, b = map(int, input().split())
-#     input_list.append(a+b)
-input_list = [8, 8, 9, 8, 9, 4, 6]
-print(input_list.index(max(input_list))+1)
+total_number = int(input())
+input_list = []
+for i in range(total_number):
+    a, b = map(int, input().split())
+    input_list.append(a+b)
+
+# input_list = [8, 8, 9, 8, 9, 4, 6]
+max_i = 0
+for i in input_list:
+    if i > 8 and i > max_i:
+        max_i = i
+
+if max_i == 0:
+    print(0)
+else:
+    print(input_list.index(max_i)+1)
