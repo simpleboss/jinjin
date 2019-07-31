18002ddce5fdae92c42260756628775aba5a2e4d
simpleboss <gichan.lee@threemindev.com>
2019-07-31 14:00:40 +0700

    input range

---
+++ jinjin.py
@@ -0,0 +1,8 @@
+# input
+# total_number = int(input())
+# input_list = []
+# for i in range(total_number):
+#     a, b = map(int, input().split())
+#     input_list.append(a+b)
+input_list = [8, 8, 9, 8, 9, 4, 6]
+print(input_list.index(max(input_list))+1)
